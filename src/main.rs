c779748ad2ed10bec319a3a72aefefad06c5f0ab
Robin Raymond <robin.raymond@taktile.com>
2023-06-27 08:12:58 +0200

    Denoise terrain

--- src/main.rs
+++ src/main.rs
@@ -15,50 +15,11 @@ fn main() {
         .run();
 }
 
-//#[derive(Component)]
-//struct Shape;
-//
-//const X_EXTENT: f32 = 14.5;
-
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    //let debug_material = materials.add(StandardMaterial {
-    //    base_color_texture: Some(images.add(uv_debug_texture())),
-    //    ..default()
-    //});
-
-    //let shapes = [
-    //    meshes.add(shape::Cube::default().into()),
-    //    meshes.add(shape::Box::default().into()),
-    //    meshes.add(shape::Capsule::default().into()),
-    //    meshes.add(shape::Torus::default().into()),
-    //    meshes.add(shape::Cylinder::default().into()),
-    //    meshes.add(shape::Icosphere::default().try_into().unwrap()),
-    //    meshes.add(shape::UVSphere::default().into()),
-    //];
-
-    //let num_shapes = shapes.len();
-
-    //for (i, shape) in shapes.into_iter().enumerate() {
-    //    commands.spawn((
-    //        PbrBundle {
-    //            mesh: shape,
-    //            material: debug_material.clone(),
-    //            transform: Transform::from_xyz(
-    //                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-    //                2.0,
-    //                0.0,
-    //            )
-    //            .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-    //            ..default()
-    //        },
-    //        Shape,
-    //    ));
-    //}
-
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 9000.0,
--- src/terrain.rs
+++ src/terrain.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use std::collections::HashMap;
 
 pub fn setup_terrain(
     mut commands: Commands,
@@ -11,12 +12,21 @@ pub fn setup_terrain(
     }
     .into();
 
+    let mut elevation = HashMap::<(i64, i64), f32>::new();
+
     for value in &mut mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION) {
-        println!("{:?}", value);
         if let bevy::render::mesh::VertexAttributeValues::Float32x3(ref mut vec) = value {
-            println!("{:?}", vec);
             for i in 0..vec.len() {
-                vec[i][1] += rand::random::<f32>() * 0.5;
+                let x = (vec[i][0] * 1000.0) as i64;
+                let z = (vec[i][2] * 1000.0) as i64;
+
+                match elevation.get(&(x, z)) {
+                    Some(e) => vec[i][1] = *e,
+                    None => {
+                        vec[i][1] = rand::random::<f32>() * 0.5;
+                        elevation.insert((x, z), vec[i][1]);
+                    }
+                }
             }
             println!("{:?}", vec);
         }
